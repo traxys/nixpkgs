6033679b3e09dfe54c73ab8995128da3ff90db0e
Jan Tojnar <jtojnar@gmail.com>
2022-07-24 08:00:27 +0200

    libpanel: init at 1.0.alpha

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19661,6 +19661,8 @@ with pkgs;
 
   libpam-wrapper = callPackage ../development/libraries/libpam-wrapper { };
 
+  libpanel = callPackage ../development/libraries/libpanel { };
+
   libpar2 = callPackage ../development/libraries/libpar2 { };
 
   libpcap = callPackage ../development/libraries/libpcap { };
