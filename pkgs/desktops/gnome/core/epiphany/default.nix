c284b43712dcd63a0c63b182a35fc0818d092bd7
Jan Tojnar <jtojnar@gmail.com>
2022-08-06 12:48:43 +0000

    epiphany: 43.alpha → 43.beta
    https://gitlab.gnome.org/GNOME/epiphany/-/compare/43.alpha...43.beta

--- pkgs/desktops/gnome/core/epiphany/default.nix
+++ pkgs/desktops/gnome/core/epiphany/default.nix
@@ -40,11 +40,11 @@
 
 stdenv.mkDerivation rec {
   pname = "epiphany";
-  version = "43.alpha";
+  version = "43.beta";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "TVm7+mQ59arqnWD94npf6GdciFP8uugPSmjZ8CjDF0E=";
+    sha256 = "TaAx7RnMtlmcrLe2mgPb+0pm7uyN2a0iR/p15qBrO34=";
   };
 
   patches = lib.optionals withPantheon [
