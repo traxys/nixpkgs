107fc801c9cc60a224f398edfa0b23b41dab7e2d
Jan Tojnar <jtojnar@gmail.com>
2022-08-15 20:14:13 +0200

    libosinfo: switch to libsoup 3
    Not compatible with programs depending on libsoup 2.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19641,9 +19641,7 @@ with pkgs;
 
   libopusenc = callPackage ../development/libraries/libopusenc { };
 
-  libosinfo = callPackage ../development/libraries/libosinfo {
-    inherit (gnome) libsoup;
-  };
+  libosinfo = callPackage ../development/libraries/libosinfo { };
 
   libosip = callPackage ../development/libraries/osip {};
 
